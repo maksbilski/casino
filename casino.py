11cf3952bc7cdde2c8c0035208989f716a8d16ba
Maks Biski <maksde25@gmail.com>
2022-12-16 21:31:39 +0100

    Implemented calculate_score and 3 helping methods.

--- casino.py
+++ casino.py
@@ -1,4 +1,5 @@
 from random import randint
+from collections import Counter
 
 
 class Casino:
@@ -24,3 +25,34 @@ class Player:
     @property
     def name(self):
         return self._name
+
+    def score_if_numbers_are_odd(self):
+        for number in self._dice_layout:
+            if number % 2 != 0:
+                return 0
+        return sum(self._dice_layout) + 2
+
+    def score_if_numbers_are_even(self):
+        for number in self._dice_layout:
+            if number % 2 == 0:
+                return 0
+        return sum(self._dice_layout) + 3
+
+    def scores_based_on_duplicates(self):
+        dice_number_count = dict(Counter(self._dice_layout))
+        score_values = []
+        for number, count in dice_number_count.items():
+            if count == 4:
+                score_values.append(number * 6)
+            if count == 3:
+                score_values.append(number * 4)
+            if count == 2:
+                score_values.append(number * 2)
+        return score_values
+
+    def calculate_score(self):
+        possible_score_values = []
+        possible_score_values.append(self.scores_based_on_duplicates)
+        possible_score_values.append(self.score_if_number_are_odd)
+        possible_score_values.append(self.score_if_numbers_are_even)
+        return max(possible_score_values)
